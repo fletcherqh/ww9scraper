d395d6f3ce1b13b1fcdb7382aaede25a9e2fe048
fletcherh <fqh1215@gmail.com>
2022-08-12 15:31:28 -0400

    downloads images to folder, works with related domains

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import argparse
 import sys
+import urllib.request
 
 
 def mainPage(url): #will get list of every chapter from the main page
@@ -36,7 +37,8 @@ def mainPage(url): #will get list of every chapter from the main page
             
             subUrl = i['href']
             
-            if subUrl == "https://ww9.readonepiece.com/chapter/one-piece-chapter-" + args.chapter:
+            #if subUrl == "https://ww9.readonepiece.com/chapter/one-piece-chapter-" + args.chapter:
+            if subUrl == "https://ww2.readkaguyasama.com/chapter/kaguya-sama-love-is-war-chapter-" + args.chapter:
 
                 print(f"Found chapter {args.chapter}!")
                 
@@ -57,8 +59,15 @@ def subPage(url): #gets every image from the sub-page
 
     print('Getting images...')
     
+    pagenum = 0
     for i in myList:
-        print(i['src'])
+        pagenum += 1
+        image_url = i['src']
+        print(image_url)
+
+        img_data = requests.get(image_url).content
+        with open(f'../../Desktop/Comics/page{pagenum}.jpg', 'wb') as handler:
+            handler.write(img_data)
 
 
 if __name__ == "__main__":
@@ -67,5 +76,6 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--chapter', help='Specific chapter to download. Format: [00X, 0XX, XXX, XXXX]')
     args = parser.parse_args()
     
+    ww2url = "https://ww2.readkaguyasama.com/manga/kaguya-sama-love-is-war/"
     ww9url = "https://ww9.readonepiece.com/manga/one-piece/"
-    mainPage(ww9url)
+    mainPage(ww2url)
