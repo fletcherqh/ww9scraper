b4e785fda87f9c1d7f1a259f99b9a440cd404650
fletcherh <fqh1215@gmail.com>
2022-08-18 21:46:26 -0400

    binary search attempts not really working that well

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ def mainPage(url): #will get list of every chapter from the main page
     soup = BeautifulSoup(site.content, 'html.parser')
 
     myList = soup.findAll('a', attrs={'class':'text md:text-lg font-bold mb-1 text-text-normal'})
-
-    chapter = loadGUI()
+    myList = enumerate(myList[::-1]) #reverse list from bottom to top and enumerate
+    #chapter = loadGUI()
+    chapter = input()
 
     if chapter == None: #Will go through all chapters in order, then ask if you want to view. type stop to break loop
-        for i in myList[::-1]:
+        for i in myList:
             
             subUrl = i['href']
             
@@ -36,19 +37,24 @@ def mainPage(url): #will get list of every chapter from the main page
     
     else:  #allows you to pick a specific chapter
         print("Looking for selected chapter...")
-        for i in myList[::-1]:
-            
-            subUrl = i['href']
-            
-            #if subUrl == "https://ww9.readonepiece.com/chapter/one-piece-chapter-" + args.chapter:
-            if subUrl == "https://ww2.readkaguyasama.com/chapter/kaguya-sama-love-is-war-chapter-" + chapter:
-
-                print(f"Found chapter {args.chapter}!")
-                
-                subPage(subUrl)
-                sys.exit(1)
-            
-        print(f"Could not find chapter {args.chapter}")
+        
+        webpage = "https://ww2.readkaguyasama.com/chapter/kaguya-sama-love-is-war-chapter-"
+        
+        chapter = webpage + chapter
+        binarySearch(myList, chapter, 0, len(myList)-1)
+        
+        #for i in myList:
+        #    
+        #    subUrl = i['href']
+        #    
+        #    if subUrl == "https://ww2.readkaguyasama.com/chapter/kaguya-sama-love-is-war-chapter-" + chapter:
+#
+        #        print(f"Found chapter {args.chapter}!")
+        #        
+        #        subPage(subUrl)
+        #        sys.exit(1)
+        #    
+        #print(f"Could not find chapter {args.chapter}")
 
 
 
@@ -71,6 +77,24 @@ def subPage(url): #gets every image from the sub-page
         img_data = requests.get(image_url).content
         with open(f'../../Desktop/Comics/page{pagenum}.jpg', 'wb') as handler:
             handler.write(img_data)
+
+def binarySearch(arr, left, right, chapter):
+    #base case
+    if r >= l:
+ 
+        mid = left + (right - left) // 2
+ 
+        if arr[mid]['href'] == chapter:
+            return arr[mid]
+ 
+        elif arr[mid]['href'] > chapter:
+            return binarySearch(arr, left, mid-1, x)
+ 
+        else:
+            return binarySearch(arr, mid+1, right, x)
+ 
+    else: #if element not in array
+        return -1
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,5 @@
+oldList = [6, 12 ,"cow"]
+
+myList = enumerate(oldList)
+
+print(myList[0])
