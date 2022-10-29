14a2180f18b024ee16d01c271b897248d738dc7f
fletcherh <fqh1215@gmail.com>
2022-10-29 16:47:33 -0400

    stable build

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-from random import randint
 from bs4 import BeautifulSoup
 import requests
 import argparse
 import sys
 import urllib.request
-from gui import loadGUI
+#from gui import loadGUI
 
 
 def mainPage(url): #will get list of every chapter from the main page
-
     site = requests.get(url)
 
     soup = BeautifulSoup(site.content, 'html.parser')
 
     myList = soup.findAll('a', attrs={'class':'text md:text-lg font-bold mb-1 text-text-normal'})
-    myList = enumerate(myList[::-1]) #reverse list from bottom to top and enumerate
-    #chapter = loadGUI()
-    chapter = input()
+    myList = (myList[::-1]) #reverse list from bottom to top and enumerate
 
-    if chapter == None: #Will go through all chapters in order, then ask if you want to view. type stop to break loop
+    if args.chapter == None: #Will go through all chapters in order, then ask if you want to view. type stop to break loop
         for i in myList:
             
             subUrl = i['href']
@@ -38,15 +34,11 @@ def mainPage(url): #will get list of every chapter from the main page
     else:  #allows you to pick a specific chapter
         print("Looking for selected chapter...")
         
-        #webpage = "https://ww2.readkaguyasama.com/chapter/kaguya-sama-love-is-war-chapter-"
-        
-        #chapter = webpage + chapter
-        
         for i in myList:
             
             subUrl = i['href']
             
-            if subUrl == "https://ww2.readkaguyasama.com/chapter/kaguya-sama-love-is-war-chapter-" + chapter:
+            if subUrl == "https://ww9.readonepiece.com/chapter/one-piece-chapter-" + args.chapter:
 
                 print(f"Found chapter {args.chapter}!")
                 
@@ -74,7 +66,7 @@ def subPage(url): #gets every image from the sub-page
         print(image_url)
 
         img_data = requests.get(image_url).content
-        with open(f'../../Desktop/Comics/page{pagenum}.jpg', 'wb') as handler:
+        with open(f'{args.path}/page{pagenum}.jpg', 'wb') as handler:
             handler.write(img_data)
 
 def binarySearch(arr, left, right, chapter):
@@ -98,10 +90,13 @@ def binarySearch(arr, left, right, chapter):
 
 if __name__ == "__main__":
     
-    #parser = argparse.ArgumentParser(description='Download all images of a chapter from ww9')
-    #parser.add_argument('-c', '--chapter', help='Specific chapter to download. Format: [00X, 0XX, XXX, XXXX]')
-    #args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Download all images of a chapter from ww9')
+    parser.add_argument('-c', '--chapter', help='Specific chapter to download. Format: [00X, 0XX, XXX, XXXX]')
+    parser.add_argument('-u', '--url', help='url of main page')
+    parser.add_argument('-p', '--path', help='path to download files')
+    args = parser.parse_args()
     
     ww2url = "https://ww2.readkaguyasama.com/manga/kaguya-sama-love-is-war/"
     ww9url = "https://ww9.readonepiece.com/manga/one-piece/"
-    mainPage(ww2url)
+
+    mainPage(args.url)
